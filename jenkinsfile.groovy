bf78579a957ad06db7953b53f8013d04cbfbf13d
Luis Moreyra <luis.moreyra.reyna@gmail.com>
2019-08-30 17:04:13 -0500

    Agregando stages

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,6 +4,29 @@ node {
         git credentialsId: 'luis.moreyra.reyna@gmail.com', url: 'https://github.com/luismoreyra/facturacion-web-taller.git'
     }
 
+    stage('Compilar') {
+        bat 'mvn clean compile'
+    }
+
+    stage('Pruebas Unitarias') {
+        echo 'Ejecutando Pruebas Unitarias ...'
+        bat 'mvn test'
+        junit '**/target/surefire-reports/*.xml'
+    }
+
+    stage('Pruebas de Integracion') {
+        echo 'Ejecutando Pruebas de Integracion ...'
+        bat 'mvn verify -DskipUTs=true'
+        junit '**/target/failsafe-reports/*.xml'
+    }
+
+    stage('Analisis de codigo estatico') {
+        echo 'Analizando codigo con SonarQube ...'
+        withSonarQubeEnv('sonar-devops') {
+            bat 'mvn sonar:sonar'
+        }
+    }
+
 }
 
 
